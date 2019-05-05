3db06ebd48a7a11f37636b2c7b74eceb6c1c58bc
Renandika Galih <rengalih666@gmail.com>
2019-05-05 17:31:31 +0700

    Updates Tipe Produk

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include $tmp_folder.'header.php';
 include $tmp_folder.'sidebar.php';
 echo '<!-- Content Wrapper. Contains page content -->';
 echo '<div class="content-wrapper">';
-	include $tmp_folder.'isi.php';
+	include $tmp_folder.'routes.php';
 echo '</div>';
 include $tmp_folder.'footer.php';
 include $tmp_folder.'footer_js.php';
